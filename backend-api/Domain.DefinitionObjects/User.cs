4f334008499b9ec27e58ee17aa2427471ede5b1a
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-08 17:43:16 +0200

    definition objects

---
+++ backend-api/Domain.DefinitionObjects/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.DefinitionObjects
+{
+    public class User
+    {
+        [Key]
+        private int id { get; set; }
+        private int familyId { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Role { get; set; }
+    }
+}
