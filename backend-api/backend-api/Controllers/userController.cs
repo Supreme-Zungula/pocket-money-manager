7622626915301c598c9f0e9eec2005a530f2294a
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-08 17:34:27 +0200

    user controller added

---
+++ backend-api/backend-api/Controllers/userController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_api.Controllers
+{
+    public class userController
+    {
+    }
+}
