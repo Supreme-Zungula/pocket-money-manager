154e9c691a1ff3456ee21e1b7e1c201c8e8bf4a3
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-11 15:56:37 +0200

    api get route fixed

--- backend-api/backend-api/Controllers/userController.cs
+++ backend-api/backend-api/Controllers/userController.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend_api.Models;
+using Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace backend_api.Controllers
 {
-    public class userController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private static List<UserModel> user = new List<UserModel> { new UserModel { FamilyId = 1, Id = 2, FirstName = "Test", LastName = "test", Relationship = "man" } };
+        private readonly SetupDB set = new SetupDB();
+
+        [Obsolete]
+        public ActionResult<List<UserModel>> Get()
+        {
+            set.Setup();
+            return Ok(user);
+        }
     }
 }
