01b485b1b6ab5af048aa48f269847c194fd8bd88
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-11 16:05:14 +0200

    userData.cs

--- backend-api/Data/userData.cs
+++ backend-api/Data/userData.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Data
 {
-    class userData
+    public class UserData
     {
+        public int Id { get; set; }
+        public int FamilyId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Relationship { get; set; }
     }
 }
