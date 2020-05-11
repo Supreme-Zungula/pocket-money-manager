46ec9d51c48d95f9f7eab0f7541a18bcc89f6b16
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-11 17:21:24 +0200

    object id added

--- backend-api/Data/userData.cs
+++ backend-api/Data/userData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,7 @@ namespace Data
 {
     public class UserData
     {
-        public int Id { get; set; }
+        public ObjectId Id { get; set; }
         public int FamilyId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
