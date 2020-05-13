a947527013d3b05298cc05c80bf2817f44783a10
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-13 11:06:35 +0200

    get database collections, users, transactions, family members etc

---
+++ backend-api/Repositories/GetDB.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    public class GetDB
+    {
+        //get the latest database, returns a collection
+        public IMongoCollection<UserData> Connect()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            //MongoServer server = client.GetServer();
+            var db = client.GetDatabase("PocketMoneyDB");
+            
+            return db.GetCollection<UserData>("Users");
+        }
+    }
+}
