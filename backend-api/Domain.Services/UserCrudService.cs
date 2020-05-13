68e371ce903785f69df086002163adce844444c4
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-13 11:04:54 +0200

    register service added

--- backend-api/Domain.Services/UserCrudService.cs
+++ backend-api/Domain.Services/UserCrudService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Data;
+using Domain.DefinitionObjects;
+using Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,10 @@ namespace Domain.Services
 {
     public class UserCrudService
     {
+        private readonly UserRepositories repositories = new UserRepositories(); 
+        public void Register(User userDetails)
+        {
+            repositories.RegisterUser(userDetails);
+        }
     }
 }
