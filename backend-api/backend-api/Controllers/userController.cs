a5f0a9534f35b1e57ada7fd1ba38e13be028eff2
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-13 11:03:58 +0200

    post method added

--- backend-api/backend-api/Controllers/userController.cs
+++ backend-api/backend-api/Controllers/userController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using backend_api.Models;
+using Domain.DefinitionObjects;
 using Domain.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace backend_api.Controllers
 {
@@ -13,14 +16,26 @@ namespace backend_api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private UserCrudService userService = new UserCrudService();
         private static readonly List<UserModel> user = new List<UserModel> { new UserModel { FirstName = "Test", LastName = "test", Role = "man" } };
         private readonly SetupDB set = new SetupDB();
 
         [Obsolete]
         public ActionResult<List<UserModel>> Get()
         {
-            set.Setup();
+            //set.Setup();
+            User use = new User { FirstName = "tess", LastName = "dfdfadf", Password = "ddfdfdf", Phone = "000099888", Role = "dhdhd" };
+            //userService.Register(use);
             return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult Post(UserModel userDetails)
+        {
+            userDetails.FamilyId = ObjectId.GenerateNewId().Increment;
+            userService.Register(userDetails.ToDomain());
+            var resourceUrl = Path.Combine(Request.Path.ToString(), Uri.EscapeUriString(userDetails.FirstName));
+            return Created(resourceUrl, userDetails);
         }
     }
 }
