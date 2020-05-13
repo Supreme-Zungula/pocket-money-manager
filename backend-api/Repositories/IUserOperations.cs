55c2dba14b13ee7fa5702d93bccdf3b2c88e811a
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-13 11:07:24 +0200

    user operations interface

---
+++ backend-api/Repositories/IUserOperations.cs
@@ -0,0 +1,15 @@
+﻿using Domain.DefinitionObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    interface IUserOperations
+    {
+        void RegisterUser(User userDetails);
+        void UserLogin(User userDetails);
+        void UpdateUser(User useDetails);
+        void DeleteUser(User useDetails);
+    }
+}
