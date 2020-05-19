23ff3354742553b0b524339e6d1d32ea4f3dc4fe
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-19 14:51:06 +0200

    FamilyMember.cs

---
+++ backend-api/Domain.DefinitionObjects/FamilyMember.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.DefinitionObjects
+{
+    public class FamilyMember
+    {
+        [Key]
+        public ObjectId Id { get; set; }
+        public int FamilyId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Relationship { get; set; }
+        [Required]
+        [MinLength(10)]
+        public string Phone { get; set; }
+    }
+}
