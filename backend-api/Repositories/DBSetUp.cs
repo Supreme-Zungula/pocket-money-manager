2a98856611f370c1722a717e4a0a8a813542a04a
Tafadzwa Muzerengani <tafadzwa@bbd.co.za>
2020-05-19 14:43:50 +0200

    family members repositories added

--- backend-api/Repositories/DBSetUp.cs
+++ backend-api/Repositories/DBSetUp.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Data;
 using Domain.DefinitionObjects;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -18,12 +19,32 @@ namespace Repositories
             MongoDatabase db = server.GetDatabase("PocketMoneyDB");
 
             UsersCollection(db);
+            FamilyMembersCollection(db);
+        }
+
+        public void FamilyMembersCollection(MongoDatabase db)
+        {
+            if (db.CollectionExists("FamilyMembers") == false)
+                db.CreateCollection("FamilyMembers");
         }
 
         public void UsersCollection(MongoDatabase db)
         {
             if (db.CollectionExists("Users") == false)
+            {
                 db.CreateCollection("Users");
+                MongoCollection<UserData> collection = db.GetCollection<UserData>("Users");
+                List<UserData> dummyUsers = new List<UserData>() 
+                {
+                    new UserData{FamilyId = ObjectId.GenerateNewId().Increment, FirstName = "Lionel", LastName = "Messi", Role = "Father", Password = "tobehashed", Phone = "0721121122"},
+                    new UserData{FamilyId = ObjectId.GenerateNewId().Increment, FirstName = "Tasha", LastName = "Cobbs", Role = "Mother", Password = "tobehashed", Phone = "0810022311"},
+                    new UserData{FamilyId = ObjectId.GenerateNewId().Increment, FirstName = "Dinelle", LastName = "Stille", Role = "Daughter", Password = "tobehashed", Phone = "0621172313"}
+                };
+                foreach(var user in dummyUsers)
+                {
+                    collection.Save(user);
+                }
+            }
         }
     }
 }
