71ea882cad51c9bd302eb0710ff8365878703222
Yamkela Zungula <yamkela@bbd.co.za>
2020-05-24 17:42:47 +0200

    Fixed bankAccount service and CORS.

--- backend-api/backend-api/Startup.cs
+++ backend-api/backend-api/Startup.cs
@@ -6,47 +6,51 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace backend_api
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly string AllowedCorsSpecific = "_allowedCorsSpecifice";
+    public Startup(IConfiguration configuration)
     {
-        private readonly string AllowedCorsSpecific = "_allowedCorsSpecifice";
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options => {
-                options.AddPolicy(name: AllowedCorsSpecific, builder => {
-                    builder.WithOrigins("http://localhost:4200");
-                });
-            });
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseCors(AllowedCorsSpecific);
-            
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddCors(options =>
+      {
+        options.AddPolicy(name: AllowedCorsSpecific, builder =>
+        {
+          builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+      });
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseCors(AllowedCorsSpecific);
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
